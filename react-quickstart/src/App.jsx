8fe2f79036cc8d6555843f18a1d2f14651f37f26
Al-Mone <allenmurphybactol@student.laverdad.edu.ph>
2024-10-09 10:49:51 -0400

    I’m done displaying data.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,22 +1,37 @@
 import './App.css'
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
 function App() {
 
   return (
     <>
-      <AboutPage />
+      <Profile />
     </>
   )
 }
 
-function AboutPage() {
+function Profile() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
+
 
 
 export default App
